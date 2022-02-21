fe0e4f88cb4a0959299df4d969f5b41536e21122
seryga-afk <seryga_94.94@mail.ru>
2022-02-22 00:36:40 +0300

    feat: decided towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if(!matrix){
+    return []
+  }else if(matrix.length > 0){
+    let arr = []
+    for (let i = 0; i < matrix.length; i++) {
+      if ((matrix.indexOf(matrix[i]) + 1) % 2 === 0) {
+        arr += "," + (matrix[i].reverse())
+      }else if((matrix.indexOf(matrix[i]) + 1) % 2 !== 0){
+        arr +=  "," + (matrix[i])
+      }
+    }
+    let arr1 = []
+    arr = arr.split(",")
+    arr.shift()
+
+    for (let i = 0; i < arr.length; i++) {
+        arr1.push(+arr[i])
+    }
+    return arr1
+  }
+    return []
 }
